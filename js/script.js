eb7da9be1df8b869f221dabc9fef19be1733e038
Seabolt Avery <150042854@cart.ads>
2017-10-31 15:26:42 -0700

    Added sticky nav

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
-$(document).ready(function() {
-  $(".js--wp-1").waypoint(function(direction) {
-    $(".js--wp-1").addClass("animated fadeIn");
-  }, {
-    offset: "50%"
-  });
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('#sticky-nav').fadeIn(500);
+        } else {
+            $('#sticky-nav').fadeOut(500);
+        }
+    });
+    $(".js--wp-1").waypoint(function (direction) {
+        $(".js--wp-1").addClass("animated fadeIn");
+    }, {
+        offset: "50%"
+    });
 });
